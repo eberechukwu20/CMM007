8ba680844cfe369e6c77f27d55898cb22c887a26
ochuba Anthony <73081724+eberechukwu20@users.noreply.github.com>
2023-03-08 11:18:32 +0000

    lab8_2

---
+++ lab6/t.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <body>
+        <p>
+        <?php
+            echo "Nigeria is a fuck";
+            ?>
+        </p>
+
+        <p>
+        <?php
+            echo "5 * 7";
+            ?>
+        </p>
+
+        <p>
+        <?php
+            $myname = "Anthony Ochuba";
+            $myage = 120;
+            echo" myname is " . $myname.  " and i am  ".  $myname;
+            ?>
+        </p>
+
+        <p>
+        <?php
+            $name = "Edgar";
+
+            if($name=="Simon"){
+                print "I kmow you!";
+            }
+            else{
+                print "Who are you?";
+            }
+            ?>
+        </p>
+
+        <p>
+        <?php
+
+        $myage = 16;
+        $myage2 = 18;
+        $myage3 = 21;
+
+        if($myage >! 16;){
+            print "buy specs if you are over " . $myage. ", ";
+        }if($myage >! 18){
+            print = "buy mugs if you are over" .$myage2. ",";
+         }
+         if($myage3 >! 21){
+            print " and buy sausage rolls if you are over ".$myage3;
+         }
+            ?>
+        </p>
+    </body>
+    </head>
+</html>
